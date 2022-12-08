4751099806d202b85c2daefa032b49efeabef8e5
Veronika181 <veronika.obrtelova@seznam.cz>
2022-12-08 17:51:15 +0100

    initial commit

---
+++ Želva_cykly/nakresli_domecek.py
@@ -0,0 +1,20 @@
+from math import sqrt
+from turtle import down, forward, left, right, exitonclick
+
+
+forward(50)
+left(135)
+forward(sqrt(50*50+50*50))
+right(135)
+forward(50)
+left(-135)
+forward(sqrt(50*50+50*50))
+right(135)
+forward(50)
+right(45)
+forward(sqrt(50*50+50*50)/2)
+right(90)
+forward(sqrt(50*50+50*50)/2)
+right(45)
+forward(50)
+print()

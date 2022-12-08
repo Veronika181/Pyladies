eabb4dd269599e51335f8a00dc36a49968c25608
Veronika181 <veronika.obrtelova@seznam.cz>
2022-12-08 16:55:50 +0100

    initial commit

---
+++ Podminky_funkce/rychlostchuze.py
@@ -0,0 +1,10 @@
+rychlost_chůze = int(input("Jaká je vaše rychlost chůze v km/h?"))
+
+if rychlost_chůze >=8:
+    print("Jsi z této planety?")
+elif rychlost_chůze >= 7:
+    print("Jsi rychlý jak zajíc")
+elif rychlost_chůze >= 5:
+            print("Chodíš opravdu rychle!")
+else:
+    print("Plazíš se jak šnek!")
